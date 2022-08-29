6bbc902c482332d09b0bdeda6b3e4eb0d419d897
vishnumeppurath <vishnu.meppurath@gmail.com>
2022-08-29 01:41:07 +0100

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get(".search-keyword").type("ca")
+    cy.get(".products").find('.product').each(($ele,index,$list)=>{
+      const textName= $ele.find("h4.product-name").text()
+      if(textName.includes("Cashews")){
+        $ele.find('button').click()
+      }
+    })
+  })
+})
